06e1afaa82e5b488e0e6d66962c7bff8e1c6e5db
Darylld Tupaz <161782745+Darylldd@users.noreply.github.com>
2024-10-25 14:07:11 +0800

    Update Admin.js

--- msuhiltsink/msuhiltsink/models/Admin.js
+++ msuhiltsink/msuhiltsink/models/Admin.js
@@ -1,15 +1,30 @@
-const db = require('../config/db');
+const db = require('../config/db'); // Import your database connection
 
 const Admin = {
-  create: (adminData, callback) => {
-    db.query('INSERT INTO admins SET ?', adminData, callback);
-  },
-  findByUsername: (username, callback) => {
-    db.query('SELECT * FROM admins WHERE username = ?', [username], (err, results) => {
-      if (err) throw err;
-      callback(null, results[0]); // Return the first result
+  create: (newAdmin, callback) => {
+    const { username, password, email } = newAdmin;
+    const query = 'INSERT INTO admins (username, password, email, createdAt) VALUES (?, ?, ?, NOW())';
+
+    db.query(query, [username, password, email], (err, result) => {
+      if (err) {
+        console.error('Error inserting admin into database:', err);
+        return callback(err);
+      }
+      callback(null, result);
     });
-  }
+  },
+
+  findByUsername: (username, callback) => {
+    const query = 'SELECT * FROM admins WHERE username = ?';
+
+    db.query(query, [username], (err, result) => {
+      if (err) {
+        console.error('Error finding admin by username:', err);
+        return callback(err);
+      }
+      callback(null, result[0]); // Return the first result
+    });
+  },
 };
 
 module.exports = Admin;
