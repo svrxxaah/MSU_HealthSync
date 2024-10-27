c5a1583ec6650e45f32e13dc2a54fc07aa83a5cd
Darylld Tupaz <161782745+Darylldd@users.noreply.github.com>
2024-10-27 21:28:28 +0800

    Update serviceController.js

--- msuhiltsink/msuhiltsink/controllers/serviceController.js
+++ msuhiltsink/msuhiltsink/controllers/serviceController.js
@@ -1,33 +1,48 @@
 const Service = require('../models/Service');
 
-exports.getServices = (req, res) => {
-  Service.getAll((err, results) => {
-    if (err) throw err;
-    res.render('services', { services: results });
-  });
+exports.getServices = async (req, res) => {
+    try {
+        const results = await Service.getAll(); // Using async/await
+        res.render('services', { services: results });
+    } catch (err) {
+        console.error('Error fetching services:', err);
+        res.status(500).send('Error fetching services');
+    }
 };
 
-exports.addService = (req, res) => {
-  const serviceData = req.body;
-  Service.create(serviceData, (err) => {
-    if (err) throw err;
-    res.redirect('/services');
-  });
+exports.addService = async (req, res) => {
+    const serviceData = req.body;
+
+    try {
+        await Service.create(serviceData); // Using async/await
+        res.redirect('/services');
+    } catch (err) {
+        console.error('Error adding service:', err);
+        res.status(500).send('Error adding service');
+    }
 };
 
-exports.updateService = (req, res) => {
-  const { id } = req.params;
-  const serviceData = req.body;
-  Service.update(id, serviceData, (err) => {
-    if (err) throw err;
-    res.redirect('/services');
-  });
+exports.updateService = async (req, res) => {
+    const { id } = req.params;
+    const serviceData = req.body;
+
+    try {
+        await Service.update(id, serviceData); // Using async/await
+        res.redirect('/services');
+    } catch (err) {
+        console.error('Error updating service:', err);
+        res.status(500).send('Error updating service');
+    }
 };
 
-exports.deleteService = (req, res) => {
-  const { id } = req.params;
-  Service.delete(id, (err) => {
-    if (err) throw err;
-    res.redirect('/services');
-  });
+exports.deleteService = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        await Service.delete(id); // Using async/await
+        res.redirect('/services');
+    } catch (err) {
+        console.error('Error deleting service:', err);
+        res.status(500).send('Error deleting service');
+    }
 };
