e7af3a95f09626b286af1f07f1909767b8edb2b1
Darylld Tupaz <161782745+Darylldd@users.noreply.github.com>
2024-10-27 21:28:04 +0800

    Update medicineController.js

--- msuhiltsink/msuhiltsink/controllers/medicineController.js
+++ msuhiltsink/msuhiltsink/controllers/medicineController.js
@@ -1,33 +1,48 @@
 const Medicine = require('../models/Medicine');
 
-exports.getMedicines = (req, res) => {
-  Medicine.getAll((err, results) => {
-    if (err) throw err;
-    res.render('medicines', { medicines: results });
-  });
+exports.getMedicines = async (req, res) => {
+    try {
+        const results = await Medicine.getAll(); // Using async/await
+        res.render('medicines', { medicines: results });
+    } catch (err) {
+        console.error('Error fetching medicines:', err);
+        res.status(500).send('Error fetching medicines');
+    }
 };
 
-exports.addMedicine = (req, res) => {
-  const medicineData = req.body;
-  Medicine.create(medicineData, (err) => {
-    if (err) throw err;
-    res.redirect('/medicines');
-  });
+exports.addMedicine = async (req, res) => {
+    const medicineData = req.body;
+
+    try {
+        await Medicine.create(medicineData); // Using async/await
+        res.redirect('/medicines');
+    } catch (err) {
+        console.error('Error adding medicine:', err);
+        res.status(500).send('Error adding medicine');
+    }
 };
 
-exports.updateMedicine = (req, res) => {
-  const { id } = req.params;
-  const medicineData = req.body;
-  Medicine.update(id, medicineData, (err) => {
-    if (err) throw err;
-    res.redirect('/medicines');
-  });
+exports.updateMedicine = async (req, res) => {
+    const { id } = req.params;
+    const medicineData = req.body;
+
+    try {
+        await Medicine.update(id, medicineData); // Using async/await
+        res.redirect('/medicines');
+    } catch (err) {
+        console.error('Error updating medicine:', err);
+        res.status(500).send('Error updating medicine');
+    }
 };
 
-exports.deleteMedicine = (req, res) => {
-  const { id } = req.params;
-  Medicine.delete(id, (err) => {
-    if (err) throw err;
-    res.redirect('/medicines');
-  });
+exports.deleteMedicine = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        await Medicine.delete(id); // Using async/await
+        res.redirect('/medicines');
+    } catch (err) {
+        console.error('Error deleting medicine:', err);
+        res.status(500).send('Error deleting medicine');
+    }
 };
