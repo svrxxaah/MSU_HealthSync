02021c97ed78d3db1c80fec6c4fc3823a9e639ef
Darylld Tupaz <161782745+Darylldd@users.noreply.github.com>
2024-11-01 23:55:13 +0800

    Update patientRoutes.js

--- msuhiltsink/msuhiltsink/routes/patientRoutes.js
+++ msuhiltsink/msuhiltsink/routes/patientRoutes.js
@@ -2,16 +2,12 @@ const express = require('express');
 const router = express.Router();
 const patientController = require('../controllers/patientController');
 
-// Get all patients
 router.get('/', patientController.getPatients);
-
-// Add new patient
 router.post('/add', patientController.addPatient);
-
-// Update a patient
 router.post('/update/:id', patientController.updatePatient);
-
-// Delete a patient
 router.post('/delete/:id', patientController.deletePatient);
+// In your router file
+router.get('/history/:schoolID', patientController.getPatientHistory);
+
 
 module.exports = router;
