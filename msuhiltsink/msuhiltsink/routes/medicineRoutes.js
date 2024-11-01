c2acc4f85be37201bf3a5bcdbedad79fbff6d48d
Darylld Tupaz <161782745+Darylldd@users.noreply.github.com>
2024-11-01 23:54:42 +0800

    Update medicineRoutes.js

--- msuhiltsink/msuhiltsink/routes/medicineRoutes.js
+++ msuhiltsink/msuhiltsink/routes/medicineRoutes.js
@@ -7,10 +7,10 @@ router.get('/', medicineController.getMedicines);
 
 // Add new medicine
 router.post('/add', medicineController.addMedicine);
-
+router.post('/edit', medicineController.updateMedicine);
 // Update a medicine
-router.post('/update/:id', medicineController.updateMedicine);
-
+router.post('/edit/:id', medicineController.updateMedicine);
+router.get('/medicines/category/:category', medicineController.getMedicinesByCategory);
 // Delete a medicine
 router.post('/delete/:id', medicineController.deleteMedicine);
 
